8b1f62465690f980b0fd6d5b257beca9590df5d1
Malcolm Jordan <105566606+mjordan513@users.noreply.github.com>
2024-04-28 20:47:36 -0400

    Delete CalculatorResults.cs

--- CalculatorResults.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CalculatorEngine;
-
-public class CalculatorResults
-{
-    public double Result { get; set; } = 0.0;
-    public bool IsSuccess { get; set; }
-    public string Operation { get; set; }
-    public string Error { get; set; }
-}

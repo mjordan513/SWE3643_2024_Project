98b29726f31dbc84bc166ffea023e91efd443671
Malcolm Jordan <105566606+mjordan513@users.noreply.github.com>
2024-04-28 20:42:49 -0400

    Add files via upload

---
+++ CalculatorResults.cs
@@ -0,0 +1,9 @@
+﻿namespace CalculatorEngine;
+
+public class CalculatorResults
+{
+    public double Result { get; set; } = 0.0;
+    public bool IsSuccess { get; set; }
+    public string Operation { get; set; }
+    public string Error { get; set; }
+}
